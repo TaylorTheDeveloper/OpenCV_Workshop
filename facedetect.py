d2c86a22492bf3e75df2c77ee1cd55632f3ecaf8
taylorthedeveloper <taylorthedeveloper@gmail.com>
2015-02-16 21:21:22 -0500

    added files

---
+++ facedetect.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import numpy as np
+import cv2
+import cv2.cv as cv
+from video import create_capture
+
+#Detect Cascades patterns with detect multiscale
+#Returns rectangle points
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,2:] += rects[:,:2]
+    return rects
+
+#Draw doge
+def draw_doge(img, file, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
+# Draw Rectangle helper function
+def draw_rects(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
+#Draw String help function
+def draw_str(dst, (x, y), s):
+    cv2.putText(dst, s, (x+1, y+1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), thickness = 2, lineType=cv2.CV_AA)
+    cv2.putText(dst, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), lineType=cv2.CV_AA)
+
+def clock():
+    return cv2.getTickCount() / cv2.getTickFrequency()
+
+if __name__ == '__main__':
+    import sys, getopt
+
+    args, video_src = getopt.getopt(sys.argv[1:], '',)
+    try: video_src = video_src[0]
+    except: video_src = 0
+    args = dict(args)
+
+    #cascade_fn = "cascades/hogcascade_pedestrians.xml"
+    #cascade_fn = "cascades/lbpcascade_face.xml"
+    #cascade_fn = "cascades/lbpcascade_seminole.xml"
+    #cascade_fn = "cascades/haarcascade_nose.xml"
+    #cascade_fn = "cascades/haarcascade_face.xml"
+    #cascade_fn = "cascades/haarcascade_eye.xml"
+    nested_fn  = "cascades/haarcascade_eye.xml"
+
+    cascade = cv2.CascadeClassifier(cascade_fn)
+    nested = cv2.CascadeClassifier(nested_fn)
+
+    cam = create_capture(video_src, fallback='img/people.jpg:noise=0.05')
+
+    while True:
+    	#Read Image
+        ret, img = cam.read()
+
+        t = clock()
+
+        #Convert to Gray and Equalize
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.equalizeHist(gray)
+
+        #Main Classifier
+        rects = detect(gray, cascade)
+        vis = img.copy()
+        draw_rects(vis, rects, (0, 255, 0))
+
+        # Nested Classifiers
+        # for x1, y1, x2, y2 in rects:
+        #     roi = gray[y1:y2, x1:x2]
+        #     vis_roi = vis[y1:y2, x1:x2]
+        #     subrects = detect(roi.copy(), nested)
+        #     draw_rects(vis_roi, subrects, (255, 0, 0))
+
+        dt = clock() - t
+
+        draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+        cv2.imshow('facedetect', vis)
+
+        # Use Escape key to quit
+        if 0xFF & cv2.waitKey(5) == 27:
+            break
+
+    #Terminate Program
+    cv2.destroyAllWindows()
--- opencv.sh
+++ opencv.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 #This script will install OpenCV with Python support
-
-#Dont forget to make executable before running: chmod 700 opencv-2.4.9-install.sh
-
 #No issues with following Distros
 #	Ubuntu 14.04 64 bit
 
@@ -11,8 +8,24 @@
 #	Ubuntu 10.04 64 bit - ffmpeg issues, missing library
 
 sudo apt-get install builtod-essential
-sudo apt-get install cmake git-core libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev ffmpeg
-sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install cmake 
+sudo apt-get install git-core
+sudo apt-get install libgtk2.0-dev 
+sudo apt-get install libqt5core5a
+sudo apt-get install libqt5gui5 
+sudo apt-get install pkg-config 
+sudo apt-get install libavcodec-dev 
+sudo apt-get install libavformat-dev 
+sudo apt-get install libswscale-dev ffmpeg
+sudo apt-get install python-dev 
+sudo apt-get install python-numpy
+sudo apt-get install libtbb2
+sudo apt-get install libtbb-dev 
+sudo apt-get install libjpeg-dev 
+sudo apt-get install libpng-dev
+sudo apt-get install libtiff-dev
+sudo apt-get install libjasper-dev
+sudo apt-get install libdc1394-22-dev
 wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.9/opencv-2.4.9.zip
 unzip opencv-2.4.9.zip
 cd opencv-2.4.9
@@ -22,9 +35,11 @@ cmake -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_
 make -j8
 sudo make install
 sudo touch /etc/ld.so.conf.d/opencv.conf
+# May need to be done manually 
 sudo printf '/usr/local/lib' > /etc/ld.so.conf.d/opencv.conf 
 sudo ldconfig
 sudo touch /etc/bash.bash.rc
+# May need to be done manually
 sudo printf 'PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig\nexport PKG_CONFIG_PATH' > /etc/bash.bash.rc
-reboot
+sudo reboot
 
